b8718a70836a41516368db745edf119c648f55b2
VovaSv <vovasvidi@gmail.com>
2023-10-19 20:51:03 +0300

    Add init logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,22 @@
-console.log('Hello!');
+'use strict';
+
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+
+function loadData() {
+    const habbitsString = localStorage.getItem(HABBIT_KEY);
+    const habbitsArray = JSON.parse(habbitsString);
+    if(Array.isArray(habbitsArray)) {
+        habbits = habbitsArray;
+        console.log(habbits)  
+    }
+}
+
+
+function saveData() {
+    const habbitsString = localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+(() => {
+    loadData();
+})()
